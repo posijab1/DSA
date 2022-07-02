e254fed23660073ab41eb9f00ebed24f039dfed9
posijab <sebastian_klix@hotmail.com>
2022-07-02 18:02:57 -0300

    02-07-2022

---
+++ Contain Duplicates.py
@@ -0,0 +1,8 @@
+def contain_duplicates(arr):
+    for i in range(len(arr)):
+        for n in range(i+1, len(arr)):
+            if arr[i] == arr[n]:
+                return True
+    return False
+
+print(contain_duplicates([1,2,3,4]))
